5024c06c574c23924b74f48c0cce64023abe2da9
Alicia <71149441+blxckwinterx@users.noreply.github.com>
2020-11-02 20:37:06 -0600

    Add files via upload

---
+++ GUIproperties.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class GUIproperties extends JFrame {
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+
+    public JButton continue_button(JPanel panel) {
+        JButton button = new JButton("Continue"); 
+        button.setPreferredSize(new Dimension(140, 60));
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        return button;
+    }   // end intro_button()
+
+    public JButton exit_button(JPanel panel) {
+        JButton button = new JButton("Exit");
+        button.setPreferredSize(new Dimension(140, 60));
+        panel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+        return button;
+    } // end intro_button()
+
+    public JLabel ascii_greeting(JPanel panel, String str) {
+        JLabel label = new JLabel();
+        panel.setLayout(new GridLayout());
+
+        return label;
+    }
+
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+}   // end class
